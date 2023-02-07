23dd86ff46e15a82e4d121d3cbd44ae1a39298d7
Vergnano Stefano <s.vergnano@reply.it>
2023-02-07 21:26:08 +0100

    implemented step 1

--- src/app/game-stats/game-stats.component.ts
+++ src/app/game-stats/game-stats.component.ts
@@ -1,27 +1,73 @@
-import { Component } from '@angular/core';
-import {Team} from '../data.models';
-import {Observable, tap} from 'rxjs';
-import {NbaService} from '../nba.service';
+import { Observable, Subscribable, Subscription, tap } from 'rxjs'
+
+import { Component, OnDestroy } from '@angular/core'
+
+import { Team } from '../data.models'
+import { NbaService } from '../nba.service'
 
 @Component({
   selector: 'app-game-stats',
   templateUrl: './game-stats.component.html',
   styleUrls: ['./game-stats.component.css']
 })
-export class GameStatsComponent {
+export class GameStatsComponent implements OnDestroy{
 
-  teams$: Observable<Team[]>;
+  subscription: Subscription
   allTeams: Team[] = [];
+  filteredTeams: Team[] = [];
+  conferences: string[] = []
+  divisions: string[] = []
+
+  selectedConference: string = ''
+  selectedDivision: string = ''
 
   constructor(protected nbaService: NbaService) {
-    this.teams$ = nbaService.getAllTeams().pipe(
-      tap(data => this.allTeams = data)
-    );
+    this.subscription = nbaService.getAllTeams().subscribe((teams)=>{
+      this.allTeams = teams
+      this.filterConferences()
+      this.filterDivisions()
+      this.filterTeams()
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe()
   }
 
   trackTeam(teamId: string): void {
-    let team = this.allTeams.find(team => team.id == Number(teamId));
+    let team = this.filteredTeams.find(team => team.id == Number(teamId));
     if (team)
       this.nbaService.addTrackedTeam(team);
+  }
+
+  onChangeConference(){
+    this.filterDivisions()
+    this.filterTeams()
+  }
+
+  onChangeDivision(){
+    this.filterTeams()
+  }
+
+  private filterConferences(){
+    this.conferences = [...new Set(this.allTeams.map(team => team?.conference))]
+  }
+
+  private filterDivisions(){
+    this.divisions = [...new Set(this.allTeams
+      .filter((team)=>!this.selectedConference || team?.conference === this.selectedConference)
+      .map(team => team?.division)
+    )]
+    if(!this.divisions.find((div)=>div===this.selectedDivision)){
+      this.selectedDivision = ''
+    }
+  }
+
+  private filterTeams(){
+    this.filteredTeams = this.allTeams.filter(
+      (team)=>
+      (!this.selectedDivision || team?.division === this.selectedDivision) &&
+      (!this.selectedConference || team?.conference === this.selectedConference)
+    )
   }
 }
